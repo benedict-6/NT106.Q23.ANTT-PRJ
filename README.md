3f0cc827227fcc9ce128e2ad6f4badfb17f6e6fb
Nguyen Luu An <benedictwork06@gmail.com>
2026-05-06 00:26:38 +0700

    loadbalancer conf, readme conf

--- README.md
+++ README.md
@@ -6,3 +6,17 @@
 4. Create pull request to merger your code to main branch.
 5. Admin will accept your require, review and merger your code into main branch.
 
+## Run project:
+
+# Run UI:
+cd "src/AdminDashboard"
+npm install
+npm run dev
+
+# Run all Server:
+npm install
+npm run dev
+
+# Run nginx:
+*Copy the nginx.conf.example to /etc/nginx/sites-available/default*
+sudo systemctl start nginx 
--- src/Server/worker/server_woker.js
+++ src/Server/worker/server_woker.js
@@ -14,15 +14,15 @@ const httpServer = createServer(app);
 app.use(cors());
 app.use(express.json());
 
+initMasterWebSocket('ws://localhost:6000');
+
 // POST /api/agent/upload — Agent gửi dữ liệu (bảo vệ bởi session token)
 app.use("api/agent/upload", verifyAgentSession, (req, res) => {
     res.sendStatus(200);
 });
 
-initMasterWebSocket('ws://localhost:6000');
-
 // Khởi động Server
-const PORT = process.env.PORT_WORKER || 3001;
+const PORT = process.env.PORT_WORKER1 || process.env.PORT_WORKER2 || process.env.PORT_WORKER3;
 
 httpServer.listen(PORT, () => {
     console.log(`Server Worker running on port ${PORT}`);
